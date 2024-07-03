b96b82d15fd77af19f04a08de1c0ae7d894c07ed
Ahmed Khan <56183507+amd858@users.noreply.github.com>
2024-07-03 16:43:38 +0500

    Delete output directory

--- output/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-D:\workspace\ArduinoProjects\weather\ station\summer_17_12_21\weather_Station_without_rf\output\core\WString.cpp.o: \
- C:\Users\5491\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\WString.cpp \
- C:\Users\5491\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\WString.h
